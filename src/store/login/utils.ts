6b7b592de29872ae17a9d221fbff9718e8993977
almo <smalljiucai@jojo.exchange>
2025-08-05 10:05:19 +0800

    fix: login callback url

--- src/store/login/utils.ts
+++ src/store/login/utils.ts
@@ -1,6 +1,6 @@
 import { tgLoginConfig } from './login.d'
 
 export function getTgLoginUrl() {
-  const redirectUri = encodeURIComponent(`${window.location.origin}?login=1`)
+  const redirectUri = encodeURIComponent(`${window.location.href}?login=1`)
   return `https://oauth.telegram.org/auth?bot_id=${tgLoginConfig.botId}&origin=${redirectUri}&request_access=write&return_to=${redirectUri}`
 }
