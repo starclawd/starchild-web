d7e23a1163592c03f3424bb53b7850d3fe8be1f3
almo <smalljiucai@jojo.exchange>
2025-08-13 22:31:00 +0800

    refactor: delete log

--- src/store/home/hooks.ts
+++ src/store/home/hooks.ts
@@ -32,7 +32,6 @@ export function useGetCandidateStatus() {
     async (account: string) => {
       try {
         const data = await triggerGetCandidateStatus({ account })
-        console.log('🔑 useGetCandidateStatus', data)
         if (data.isSuccess) {
           setCandidateStatus(data.data)
         }
