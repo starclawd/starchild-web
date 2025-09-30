319829dea34f9a3bafb11419b5cbe960addc5e3b
almo <smalljiucai@jojo.exchange>
2025-09-30 19:47:37 +0800

    fix: tglogin test

--- src/components/Header/components/TgLogin/index.tsx
+++ src/components/Header/components/TgLogin/index.tsx
@@ -10,25 +10,23 @@ const TgLoginWrapper = styled.div`
 
 export const TgLogin = ({ onAuth, size = 'small' }: TelegramLoginButtonProps) => {
   useEffect(() => {
-    // 挂载回调函数
-    ;(window as any).onTelegramAuth = (user: TelegramUser) => {
-      window.tgUserInfo = user
-      onAuth(user)
-    }
-
-    const script = document.createElement('script')
-    script.src = 'https://telegram.org/js/telegram-widget.js?7'
-    script.setAttribute('data-telegram-login', tgLoginConfig.username)
-    script.setAttribute('data-size', size)
-    script.setAttribute('data-userpic', 'false')
-    script.setAttribute('data-request-access', 'write')
-    script.setAttribute('data-onauth', 'onTelegramAuth(user)')
-    script.setAttribute('data-lang', 'en')
-    script.async = true
-    document.getElementById('telegram-login')?.appendChild(script)
-
-    return () => {
-      document.getElementById('telegram-login')?.replaceChildren()
+    const container = document.getElementById('telegram-login')
+    if (container && !container.hasChildNodes()) {
+      // 挂载回调函数
+      ;(window as any).onTelegramAuth = (user: TelegramUser) => {
+        window.tgUserInfo = user
+        onAuth(user)
+      }
+      const script = document.createElement('script')
+      script.src = 'https://telegram.org/js/telegram-widget.js?7'
+      script.setAttribute('data-telegram-login', tgLoginConfig.username)
+      script.setAttribute('data-size', size)
+      script.setAttribute('data-userpic', 'false')
+      script.setAttribute('data-request-access', 'write')
+      script.setAttribute('data-onauth', 'onTelegramAuth(user)')
+      script.setAttribute('data-lang', 'en')
+      script.async = true
+      container.appendChild(script)
     }
   }, [onAuth, size])
 
