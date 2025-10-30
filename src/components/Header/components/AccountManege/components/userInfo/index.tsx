7101e6f4d24b69fae070d2589a5c7beb562ee0cb
almo <smalljiucai@jojo.exchange>
2025-10-30 11:22:02 +0800

    refactor: account magnege

--- src/components/Header/components/AccountManege/components/userInfo/index.tsx
+++ src/components/Header/components/AccountManege/components/userInfo/index.tsx
@@ -1,9 +1,119 @@
+import { Trans } from '@lingui/react/macro'
+import Avatar from 'boring-avatars'
+import { IconBase } from 'components/Icons'
+import { ANI_DURATION } from 'constants/index'
+import { useUserInfo } from 'store/login/hooks'
 import styled from 'styled-components'
 
 const UserInfoWrapper = styled.div`
   display: flex;
+  align-items: center;
+  gap: 12px;
+`
+
+const RightContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  flex-grow: 1;
+`
+
+const UserName = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  font-size: 18px;
+  font-weight: 500;
+  line-height: 26px;
+  color: ${({ theme }) => theme.textL1};
+`
+
+const IconWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 20px;
+  height: 20px;
+  font-size: 14px;
+  color: ${({ theme }) => theme.textL4};
+  cursor: pointer;
+  transition: all ${ANI_DURATION}s;
+  &:hover {
+    color: ${({ theme }) => theme.textL1};
+  }
+`
+
+const Bottom = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+const Uid = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  > span {
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 20px;
+    color: ${({ theme }) => theme.textL3};
+    span {
+      color: ${({ theme }) => theme.textL1};
+    }
+  }
+`
+
+const Primay = styled.div`
+  display: flex;
+  align-items: center;
+  width: fit-content;
+  height: 20px;
+  padding: 0 6px;
+  gap: 4px;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 18px;
+  border-radius: 20px;
+  background: ${({ theme }) => theme.text20};
+  span:first-child {
+    color: ${({ theme }) => theme.textL3};
+  }
+  span:last-child {
+    color: ${({ theme }) => theme.textL1};
+  }
 `
 
 export default function UserInfo() {
-  return <UserInfoWrapper></UserInfoWrapper>
+  const [{ telegramUserName, telegramUserId }] = useUserInfo()
+  return (
+    <UserInfoWrapper>
+      <Avatar size={40} name={telegramUserName} />
+      <RightContent>
+        <UserName>
+          <span>{telegramUserName}</span>
+          <IconWrapper>
+            <IconBase className='icon-edit' />
+          </IconWrapper>
+        </UserName>
+        <Bottom>
+          <Uid>
+            <span>
+              <Trans>User ID</Trans>: <span>{telegramUserId}</span>
+            </span>
+            <IconWrapper>
+              <IconBase className='icon-chat-copy' />
+            </IconWrapper>
+          </Uid>
+          <Primay>
+            <span>
+              <Trans>Primary</Trans>:
+            </span>
+            <span>Google</span>
+          </Primay>
+        </Bottom>
+      </RightContent>
+    </UserInfoWrapper>
+  )
 }
--- src/components/Header/components/AccountManege/index.tsx
+++ src/components/Header/components/AccountManege/index.tsx
@@ -6,6 +6,13 @@ import { ModalSafeAreaWrapper } from 'components/SafeAreaWrapper'
 import { Trans } from '@lingui/react/macro'
 import { vm } from 'pages/helper'
 import BottomSheet from 'components/BottomSheet'
+import Divider from 'components/Divider'
+import UserInfo from './components/userInfo'
+import { useMemo } from 'react'
+import Google from './components/Google'
+import Telegram from './components/Telegram'
+import googleIcon from 'assets/media/google.png'
+import { IconBase } from 'components/Icons'
 
 const AccountManegeWrapper = styled.div`
   display: flex;
@@ -55,17 +62,99 @@ const Content = styled.div`
   padding: 20px 0;
 `
 
+const Title = styled.div`
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 20px;
+  color: ${({ theme }) => theme.textL3};
+`
+
+const LoginMethods = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`
+
+const LoginMethod = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+const Left = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 20px;
+  color: ${({ theme }) => theme.textL3};
+  i {
+    font-size: 18px;
+  }
+  img {
+    width: 18px;
+    height: 18px;
+  }
+`
+
+const Right = styled.div``
+
 export function AccountManegeModal() {
   const isMobile = useIsMobile()
   const accountManegeModalOpen = useModalOpen(ApplicationModal.ACCOUNT_MANEGE_MODAL)
   const toggleAccountManegeModal = useAccountManegeModalToggle()
+  const loginMethods = useMemo(() => {
+    return [
+      {
+        key: 'Google',
+        name: <Trans>Google</Trans>,
+        icon: <img src={googleIcon} alt='Google' />,
+        value: <Google />,
+      },
+      {
+        key: 'Telegram',
+        name: <Trans>Telegram</Trans>,
+        icon: <IconBase className='icon-telegram' />,
+        value: <Telegram />,
+      },
+      {
+        key: 'Wallet',
+        name: <Trans>Wallet</Trans>,
+        icon: <IconBase className='icon-account-wallet' />,
+        value: '',
+      },
+    ]
+  }, [])
   const renderContent = () => {
     return (
       <>
         <Header>
           <Trans>Account management</Trans>
         </Header>
-        <Content></Content>
+        <Content>
+          <Title>
+            <Trans>Profile Summary</Trans>
+          </Title>
+          <Divider paddingVertical={20} paddingHorizontal={0} />
+          <UserInfo />
+          <Divider paddingVertical={20} paddingHorizontal={0} />
+          <LoginMethods>
+            {loginMethods.map((item) => {
+              const { key, name, icon, value } = item
+              return (
+                <LoginMethod key={key}>
+                  <Left>
+                    {icon}
+                    <span>{name}</span>
+                  </Left>
+                  <Right>{value}</Right>
+                </LoginMethod>
+              )
+            })}
+          </LoginMethods>
+        </Content>
       </>
     )
   }
